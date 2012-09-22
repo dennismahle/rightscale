5110e86b68547615e77be4a162c04dd32c424581
dennismahle <dennis@watchwith.com>
2012-09-22 01:50:18 +0000

    Added a hello world message to the default.rb recipe

---
+++ cookbooks/cookbook2/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "Watchwith"
+maintainer_email "dennis@watchwith.com"
+license          "All rights reserved"
+description      "Installs/Configures cookbook2"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+recipe "cookbook2::default","Prints hello world output"
