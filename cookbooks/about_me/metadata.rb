d9fd5c3ab5c0a8a207f0d8f63abb96914216be6e
dennismahle <dennis@watchwith.com>
2012-09-22 04:23:08 +0000

    Added the about_me cookbook

---
+++ cookbooks/about_me/metadata.rb
@@ -0,0 +1,11 @@
+maintainer       "Watchwith"
+maintainer_email "dennis@watchwith.com"
+license          "All rights reserved"
+description      "Installs/Configures about_me"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+depends "rightscale"
+
+recipe "about_me::default","Prints my first name and several of my favorite things as output."
+
