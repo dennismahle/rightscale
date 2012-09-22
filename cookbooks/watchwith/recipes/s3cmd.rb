f86eb594992372982ddb671afe877de4c8058239
dennismahle <dennis@watchwith.com>
2012-09-22 18:36:58 +0000

    fixed recipe?

--- cookbooks/watchwith/recipes/s3cmd.rb
+++ cookbooks/watchwith/recipes/s3cmd.rb
@@ -21,8 +21,8 @@ bash "Activate environment" do
   export AWS_CREDENTIALS_FILE="/root/.aws_credentials"
   export AWS_ACCESS_KEY_ID="#{node[:watchwith][:s3cmd][:aws_key_id]}"
   export AWS_SECRET_ACCESS_KEY="#{node[:watchwith][:s3cmd][:aws_key_secret]}"
-  echo AWS_CREDENTIALS_FILE: $AWS_CREDENTIALS_FILE
-  echo AWS_ACCESS_KEY_ID: $AWS_SECRET_ACCESS_KEY
+  echo AWS_ACCESS_KEY_ID: $AWS_ACCESS_KEY_ID
+  echo AWS_SECRET_ACCESS_KEY: $AWS_SECRET_ACCESS_KEY
   s3cmd get "s3://watchwith-wars/test_tgz/watchwith-api_20120817-1056.tar.gz" api.tar.gz
   EOH
 end
